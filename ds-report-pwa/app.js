d63be8f0973d4d9c3d10f0cf63408cdd388628d3
Rayken F. <rayken.fang@gmail.com>
2026-03-10 00:54:50 +0800

    Update app.js

--- ds-report-pwa/app.js
+++ ds-report-pwa/app.js
@@ -1,11 +1,27 @@
 function submitTest(){
 
 const data = {
-
 date:"2026-03-09",
 person:"測試",
 site:"PWA",
 work:"測試提交"
-
 }
+
+fetch("https://script.google.com/macros/s/AKfycbzjwQZZn2nkko-WtLUSfY1_1xfgOXnESt6kQ-SMLlbFDthIowENSoXfrOhMH-dDCy7wPQ/exec",{
+method:"POST",
+headers:{
+"Content-Type":"application/json"
+},
+body:JSON.stringify(data)
+})
+.then(res=>res.json())
+.then(result=>{
+console.log(result)
+alert("送出成功")
+})
+.catch(err=>{
+console.error(err)
+alert("送出失敗")
+})
+
 }
