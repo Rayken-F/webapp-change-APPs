0029a8b17efe0c969b107985d115a39bc3f423e1
Rayken F. <rayken.fang@gmail.com>
2026-03-16 23:13:12 +0800

    Update api.js

--- ds-report-pwa/api.js
+++ ds-report-pwa/api.js
@@ -1,4 +1,6 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbwYjPR-mHy_UCRAAsvU84-3T_MMQcfKHX9PSR8Da7E2gQq3xVEcK0Fnz0JvrHaIHpem/exec";
+//版本43 沒問題的板
+//const API_URL = "https://script.google.com/macros/s/AKfycbwYjPR-mHy_UCRAAsvU84-3T_MMQcfKHX9PSR8Da7E2gQq3xVEcK0Fnz0JvrHaIHpem/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbxtef9i0HrL15Y0JhBPvfr_OPC9zZFMW5zC3vrKzDZg4AWVNDniOCc5LcMSRscuR-n6/exec";
 
 async function submitDailyReportAPI(payload) {
   const res = await fetch(API_URL, {
