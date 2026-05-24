0ca5332e1f38d93f6e50cd08c6b0b85ab7363ef2
Rayken F. <rayken.fang@gmail.com>
2026-05-24 15:22:37 +0800

    Restore previous API_URL and update comments

--- ds-report-pwa/api.js
+++ ds-report-pwa/api.js
@@ -1,6 +1,7 @@
 //版本43 沒問題的板
-//const API_URL = "https://script.google.com/macros/s/AKfycbwYjPR-mHy_UCRAAsvU84-3T_MMQcfKHX9PSR8Da7E2gQq3xVEcK0Fnz0JvrHaIHpem/exec";
-const API_URL = "https://script.google.com/macros/s/AKfycbxtef9i0HrL15Y0JhBPvfr_OPC9zZFMW5zC3vrKzDZg4AWVNDniOCc5LcMSRscuR-n6/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbwYjPR-mHy_UCRAAsvU84-3T_MMQcfKHX9PSR8Da7E2gQq3xVEcK0Fnz0JvrHaIHpem/exec";
+//版本59(與AP的api共用所以這是預備方案，目前不啟動)
+//const API_URL = "https://script.google.com/macros/s/AKfycbxtef9i0HrL15Y0JhBPvfr_OPC9zZFMW5zC3vrKzDZg4AWVNDniOCc5LcMSRscuR-n6/exec";
 
 async function submitDailyReportAPI(payload) {
   const res = await fetch(API_URL, {
