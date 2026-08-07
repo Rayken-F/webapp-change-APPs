20f2bdf1d28deee9fe0a2b038d188ece16c2471c
Rayken F. <rayken.fang@gmail.com>
2026-08-07 13:46:43 +0800

    Update api.js

--- DS-IQC-WIP/api.js
+++ DS-IQC-WIP/api.js
@@ -8,7 +8,7 @@
   const CLIENT_VERSION = "IQC_CORRECTION_V0_1_20260806";
 
   // 部署 Apps Script 後，將下方網址替換成固定 /exec URL。
-  const API_URL = "PASTE_YOUR_APPS_SCRIPT_EXEC_URL_HERE";
+  const API_URL = "https://script.google.com/macros/s/AKfycbzgOpdC9aStQBaqMjR9MORMFcmWi-DTbP3f_RLtb5lq_U48e1kv_7vu9z_9IHtJqQDs/exec";
 
   const TOKEN_KEY = "ds_iqcc_session_v1";
 
