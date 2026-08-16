7c2d855f3f76728b3f7da8c0003d155e9f5abd08
Rayken F. <rayken.fang@gmail.com>
2026-08-16 20:52:40 +0800

    Update service worker cache and static assets

--- ds-app/sw.js
+++ ds-app/sw.js
@@ -1,8 +1,23 @@
 "use strict";
-const CACHE="ds-app-shell-v1-20260816";
-const STATIC=["./","./index.html","./app.css","./app.js","./config.js","./manifest.webmanifest"];
-self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(STATIC)).then(()=>self.skipWaiting())));
-self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+const CACHE="ds-app-shell-v1-1-20260816";
+const STATIC=[
+  "./",
+  "./index.html",
+  "./app.css",
+  "./app.js",
+  "./config.js",
+  "./manifest.json",
+  "./portal-gate.js",
+  "./icons/apple-touch-icon.png",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+self.addEventListener("install",event=>event.waitUntil(
+  caches.open(CACHE).then(c=>c.addAll(STATIC)).then(()=>self.skipWaiting())
+));
+self.addEventListener("activate",event=>event.waitUntil(
+  caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())
+));
 self.addEventListener("fetch",event=>{
   if(event.request.method!=="GET") return;
   const url=new URL(event.request.url);
