71425d5855f4768965b6f0af5b71334b67113eb1
Rayken F. <rayken.fang@gmail.com>
2026-08-16 23:29:33 +0800

    Update config.js

--- ds-app/config.js
+++ ds-app/config.js
@@ -1,7 +1,7 @@
 (function(global){
   "use strict";
   global.DS_PORTAL_CONFIG = Object.freeze({
-    CLIENT_VERSION: "DS_APP_SHELL_V1_2_20260816",
+    CLIENT_VERSION: "DS_APP_SHELL_V1_3_20260816",
 
     AUTH_API_URL: "https://script.google.com/macros/s/AKfycbzgOpdC9aStQBaqMjR9MORMFcmWi-DTbP3f_RLtb5lq_U48e1kv_7vu9z_9IHtJqQDs/exec",
     AUTH_CLIENT_VERSION: "IQC_CORRECTION_V0_9_7_20260810",
